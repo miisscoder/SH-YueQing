d4759160e8a14321c03f66072aaf1aad5b29f736
miisscoder <misscoder@hotmail.com>
2020-10-10 21:37:30 +0800

    initial commit

---
+++ js/flexible.js
@@ -0,0 +1,42 @@
+﻿$(function ($) {
+    var tid;
+    function refreshRem() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        var widthDesign = 2285 ;
+         if (width < 1000 )  {
+            var rem = width * 100 / widthDesign;
+            $('html').css('font-size', rem + 'px');
+        }
+        if ($('.form').height() + $('.header').height() < height) {
+            $('.form').css('height', (height - $('.header').height()) + 'px');
+        }
+    }
+
+    refreshRem();
+
+    if (window.addEventListener) {
+        window.addEventListener("resize", function () {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        }, false);
+        window.addEventListener("pageshow", function (e) {
+            if (e.persisted) { // 浏览器后退的时候重新计算
+                clearTimeout(tid);
+                tid = setTimeout(refreshRem, 300);
+            }
+        }, false);
+    } else {
+        window.attachEvent("resize", function () {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        });
+        window.attachEvent("pageshow", function (e) {
+            if (e.persisted) { // 浏览器后退的时候重新计算
+                clearTimeout(tid);
+                tid = setTimeout(refreshRem, 300);
+            }
+        });
+    }
+    
+});
---
+++ js/index.js
@@ -0,0 +1,4 @@
+﻿$('.radio').click(function () {
+    $('input[name="gender"]').attr('checked', false);
+    $(this).children('input').attr('checked', 'checked');
+});
